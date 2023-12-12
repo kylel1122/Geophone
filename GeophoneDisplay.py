993c924c6051d5625ee45d0a0881c3f10cf3c48a
kylel1122 <kyleleleux@gmail.com>
2023-12-12 07:11:23 +0000

    Adding a number of buttons, comboboxes, labels that we might need. have not layed they out yet.

--- GeophoneDisplay.py
+++ GeophoneDisplay.py
@@ -1,13 +1,16 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+import Geophone
+from PyQt5.QtWidgets import QApplication, QComboBox, QCheckBox, QLineEdit QMainWindow, QLabel, QPushButton
 from PyQt5 import QtWidgets
 import Seismograph
 
 class GeophoneDisplay(QMainWindow):
 
-    def __init__(self):
+    def __init__(self, accessibility = True):
         super(GeophoneDisplay, self).__init__()
-
-
+        self.geo = Geophone()
+        
+        self.accessibility = accessibility
+        
         self.setGeometry(200, 200, 300, 300)
 
         self.setupUI()
@@ -15,3 +18,34 @@ class GeophoneDisplay(QMainWindow):
     def setupUI(self):
         self.setWindowTitle('Seismograph Main Window')
         self.createGridLayout()
+
+        # Setting up some widgets I think I will need without much else thought for right now
+        self.gainCombo = QComboBox()
+        # TODO: Need to do something with this value once it is set
+        self.gainCombo.addItems(list(self.geo.validGains))
+        self.gainCombo.currentTextChanged.connect(self.gainChange)
+
+        self.readingLabel = QLabel()
+        
+        self.sampleTime = QLineEdit()
+        self.sampleTime.returnPressed.connect(self.sampleTimeChange)
+
+        self.sampleTimeRBV = QLabel()
+        
+        self.startBtn = QPushButton()
+
+
+        self.saveLast24Btn = QPushButton()
+        # TODO: Move these to pyqt graph widget probably
+        self.zoomInBtn = QPushButton()
+        self.zoomOutBtn = QPushButton()
+        self.resetWindowBtn = QPushButton()
+        
+
+        # TODO: slider to see back throughout the day or zoom in and out
+
+    def gainChange(self):
+        self.geo.gain(int(self.gainCombo.currentText()))
+
+    def sampleTimeChange(self):
+        self.geo.sampleTime(float(self.sampleTime.currentText()))
