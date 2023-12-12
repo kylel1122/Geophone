dbc91a0ad1a167ac5795d84a08ca9b8522a8ce97
kylel1122 <kyleleleux@gmail.com>
2023-12-12 03:01:18 +0000

    Creating a class for the breakout board

---
+++ Geophone.py
@@ -0,0 +1,34 @@
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x14.analog_in import AnalogIn
+import board
+import busio
+import time
+
+class Geophone():
+
+    def __init__():
+        
+        self.init_gadc()
+
+
+    def init_gadc(self):
+        
+        i2c = busio.I2C(board.SCL, board.SDA)
+        self.ads = ADS.ADS1115(i2c)
+        self.chan = AnalogIn(ads, ADS.P1)
+
+    @property(int)
+    def adcGain(self) -> int:
+        return self._gain
+
+    @adsGain.setter(int)
+    def adsGain(self, value: int):
+        VALID_GAINS = {1, 2, 4, 8, 16}
+        if value not in VALID_GAINS:
+            raise ValueError(f'Gain value must be one of the following: {VALID_GAINS}')
+        else:
+            self._gain = value
+
+    @property(float)
+    def chanVoltage(self) -> float:
+        return self.chan.voltage
