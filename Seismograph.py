13facf214a5b116309d7970856a960a8072c4df7
kylel1122 <kyleleleux@gmail.com>
2023-12-19 20:48:24 -0800

    Working plot display with random numbers

--- Seismograph.py
+++ Seismograph.py
@@ -1,14 +1,51 @@
 import pyqtgraph as pg
-from PyQt5 import QtCore
+from PyQt5 import QtCore, QtWidgets
+from random import randint
 #import Geophone
+
+pg.setConfigOption('background', 'w')
+pg.setConfigOption('foreground', 'k')
 
 class Seismograph(pg.PlotWidget):
 
-    def __init__(self, parent=None):
+    def __init__(self, update_time=1000, parent=None):
         super(Seismograph, self).__init__(parent)
         #TODO: set up geophone here I would think
         #      need to think about this
-        self.updateTime(1.0)
+        self._updateTime = update_time
+        self.setupPlot()
+        self.setupTimer()
+
+
+    def setupPlot(self):
+        units = 'mV'
+        gain = 50
+        self.seismo = self.getPlotItem()
+        #self.setCentralWidget(self.seismo)
+        #self.seismo.setBackground('w')
+        pen = pg.mkPen(color=(255, 0, 0))
+
+        self.seismo.setTitle(f'Response ({units}) Vs Time', color='b', size='20pt')
+        styles = {'color':'red', 'font-size':'18px'}
+        self.seismo.setLabel('left', f'Response ({units})', **styles)
+        self.seismo.setLabel('bottom', f'Time (s)', **styles)
+        self.seismo.addLegend()
+        self.seismo.showGrid(x=True, y=True)
+        self.seismo.setYRange(gain, -1*gain)
+
+        self.time = list(range(10))
+        self.response = [randint(0, 40) for _ in range(10)]
+
+        self.line = self.seismo.plot(self.time,
+                                     self.response,
+                                     name='Response',
+                                     pen=pen,
+                                     symbol='+',
+                                     symbolSize=15,
+                                     symbolBrush='b',
+                                     )
+
+
 
     @property
     def updateTime(self) -> float:
@@ -30,6 +67,7 @@ class Seismograph(pg.PlotWidget):
 
         # Move buffer up by sampled data points
         # What does a full day of samples look like. should we start scrolling??
+        '''
         if len(timePlot) < fullDay:
             self.timePlot.append(self.timeBuffer)
             self.seisPlot.append(self.seisBuffer)
@@ -38,10 +76,21 @@ class Seismograph(pg.PlotWidget):
             self.timePlot.append(self.timeBuffer)
             self.seisPlot = self.seisPlot[self.updateLength:]
             self.seisPlot.append(self.seisBuffer)
+        '''
+        self.time = self.time[1:]
+        self.time.append(self.time[-1] +1)
+        self.response = self.response[1:]
+        self.response.append(randint(20,40))
+        self.line.setData(self.time, self.response)
 
     def setupTimer(self):
 
         self.timer = QtCore.QTimer()
-        self.time.setInterval(self.updateTime)
+        self.timer.setInterval(int(self.updateTime))
         self.timer.timeout.connect(self.updatePlot)
-        self.time.start()
+        self.timer.start()
+
+app = QtWidgets.QApplication([])
+main = Seismograph()
+main.show()
+app.exec()
